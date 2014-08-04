a842fb2047902460e42cc6c34382adc2e99a31b1
Denis Pismenny <denis.pismenny@gmail.com>
2014-08-04 19:30:24 +0300

    done

--- js/common.js
+++ js/common.js
@@ -1,5 +1,35 @@
 head.ready(function() {
 
-	console.log("These aren't the droids you're looking for!");
+	// Navigation
+	$('.js-nav a').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash,
+		$target = $(target);
+
+		$('html, body').stop().animate({
+				'scrollTop': $target.offset().top - 18
+		}, 500, 'swing', function () {
+				// window.location.hash = target;
+		});
+	});
+
+	// popups
+	$(".js-popup-link").on("click", function(event){
+		$(".js-overlay").fadeOut(200);
+		var popup = $(this).attr("href");
+		$("body").addClass("has-open-popup");
+		$("."+popup).parent().fadeIn(200);
+		event.stopPropagation();
+		return false;
+	});
+	$(".js-popup-close").on("click", function(){
+		$(".js-overlay").fadeOut(200);
+		$("body").removeClass("has-open-popup");
+		return false;
+	});
+	$(".js-popup").on("click", function(event){
+		event.stopPropagation();
+	});
 
 });
--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 head.load(
 	"js/lib/jquery.js",
+	"js/lib/masonry.pkgd.min.js",
 	"js/common.js"
 );
